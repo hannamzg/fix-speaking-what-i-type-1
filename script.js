a2d1c69c7103fc2e713e4f8e9a2774f05bb3a236
hanna mzeget <elimzghanna123@gmail.com>
2022-08-26 19:58:15 +0300

    try 2

---
+++ script.js
@@ -0,0 +1,85 @@
+const textarea = document.querySelector("#text");
+let voiceList = document.querySelector("#voice");
+let speechBtn = document.querySelector(".submit");
+
+let synth  = speechSynthesis;
+let isSpeaking = true;
+
+function voiceSpech(){
+    for(let voice of synth.getVoices()){
+        let option = document.createElement("option");
+        option.text += voice.name;
+        voiceList.add(option)
+    }
+}
+
+synth.addEventListener("voiceschanged", voiceSpech);
+
+ function texttospeech(text){
+    let utternance = new SpeechSynthesisUtterance(text);
+    for(let voice of synth.getVoices()){
+        if(voice.name === voiceList.value){
+            utternance.voice =voice;
+        }
+    }
+    speechSynthesis.speak(utternance);
+}
+
+
+
+speechBtn.addEventListener("click" , (e)=>{
+    e.preventDefault();
+    if(textarea.value !== ""){
+        if(!synth.speaking){
+            texttospeech(textarea.value)
+        } 
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* speechBtn.addEventListener('click' , (e)=>{
+    e.preventDefault();
+    if(textarea.value !== ""){
+        if(!synth.speaking){
+            texttospeech(textarea.value)
+        }
+        if(textarea.value.length > 80){
+            if(isSpeaking){
+                synth.resume()
+                isSpeaking = false
+                speechBtn.innerHTML = "pause speech"
+            }
+            else{
+                synth.pause();
+                isSpeaking = true;
+                speechBtn.innerHTML = "resume speech"
+            }
+            setInterval(() =>{
+                if(!synth.speaking&& !isSpeaking){
+                    isSpeaking = true;
+                    speechBtn.innerHTML = "convert to speech"
+                }
+            })
+        }else{
+            speechBtn.innerHTML = " covert to speech"
+        }
+    }
+}) */
